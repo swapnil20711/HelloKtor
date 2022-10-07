44e02c6d545557ca1d4678963fed7e0cedd8f523
Swapnil Bhojwani <swapnil.bhojwani@gmail.com>
2022-10-07 22:47:29 +0530

    Learning about url parameter and request related things ktor

--- src/main/kotlin/com/swapcodes/Application.kt
+++ src/main/kotlin/com/swapcodes/Application.kt
@@ -3,6 +3,7 @@ package com.swapcodes
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -10,12 +11,33 @@ fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         install(Routing) {
             homeRoute()
+            urlParamRoute()
         }
     }.start(wait = true)
 }
 
 fun Routing.homeRoute() {
     get("/") {
+        println(call.request.uri)
+        println(call.request.headers.names())
+        println(call.request.headers["User-Agent"])
+        println(call.request.queryParameters.names())
+        println("Name is ${call.request.queryParameters["name"]}")
+        println("Email is ${call.request.queryParameters["email"]}")
         call.respond("Hello ktor😍")
+    }
+}
+
+fun Routing.urlParamRoute() {
+    val list = listOf<String>("C", "C++", "Java", "Javascript", "kotlin", "Python", "Go Lang", "Scala")
+    get("/codingLang/{page}") {
+        val pageNumber = call.parameters["page"]
+        val fp = pageNumber!!.toInt() - 1
+        call.respondText {
+            if (fp > list.size - 1)
+                "Not found"
+            else
+                "Coding language at index $fp+1 is ${list[fp]}"
+        }
     }
 }
